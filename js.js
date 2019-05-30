5ffa59ac6642197b2235de5d7d0ebd154ac40138
RostislavSuslov <suslovros@gmail.com>
2019-05-30 10:48:38 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,22 @@
+// $(document).ready(function($){
+// function formatDate(date) {
+//   var monthNames = [
+//     "January", "February", "March",
+//     "April", "Мая", "Июня", "Июля",
+//     "August", "September", "October",
+//     "November", "December"
+//   ];
+
+// //   var day = date.getDate()+ 3;
+// //   var monthIndex = date.getMonth();
+
+// //   return day + ' ' + monthNames[monthIndex];
+// // }
+
+// 	let now = new Date();
+// 	now.setDate(now.getDate() + 1);
+// 	console.log(now);
+// 	return now.getDate() + monthNames[now.getMounth()];
+// }
+// formatDate(new Date()); 
+//$('#date').html(formatDate(new Date()));
